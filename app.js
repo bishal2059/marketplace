edf5ba89a623bcd7fecda4ca41b58ec8144a0266
bishal2059 <bishal.sap21@gmail.com>
2023-11-10 17:56:26 +0545

    deploy v1.3

--- app.js
+++ app.js
@@ -30,6 +30,15 @@ app.use(morgan("combined"));
 app.use(cookieParser());
 app.use(express.json());
 
+//CORS-cookie Headers
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONT_END_URL);
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, *");
+  res.next();
+});
+
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use((err, req, res, next) => {
